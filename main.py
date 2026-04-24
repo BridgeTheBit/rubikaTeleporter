e244576cecee4a5c585f40db9b0a7cb0f07dd61a
BridgeTheBit <mehran.ershadi@gmail.com>
2026-04-24 15:29:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-import subprocess, sys
+import subprocess
+import sys
+import signal
+import os
 
-procs = []
+processes = []
 
-try:
-    procs.append(subprocess.Popen([sys.executable, "telebot.py"]))
-    procs.append(subprocess.Popen([sys.executable, "rub.py"]))
 
-    for p in procs:
-        p.wait()
-except KeyboardInterrupt:
-    for p in procs:
-        p.kill()
+def shutdown(signum=None, frame=None):
+    for p in processes:
+        if p.poll() is None:
+            p.terminate()
+    sys.exit(0)
+
+
+def main():
+    global processes
+
+    signal.signal(signal.SIGTERM, shutdown)
+    signal.signal(signal.SIGINT, shutdown)
+
+    processes = [
+        subprocess.Popen([sys.executable, "telebot.py"]),
+        subprocess.Popen([sys.executable, "rub.py"]),
+    ]
+
+    while True:
+        for p in processes:
+            if p.poll() is not None:
+                shutdown()
+        signal.pause()
+
+
+if __name__ == "__main__":
+    main()
